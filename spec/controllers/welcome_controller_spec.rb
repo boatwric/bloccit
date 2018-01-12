1ba8f6a81b82e9601cb6a93be4a1597aaefaa69b
Chris Boatwright <christopher.boatwright@gmail.com>
2018-01-11 19:50:41 -0500

    Checkpoint Rails-Testing Commit 01

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do #describes subject of spec, WelcomeController
+    
+    describe "GET index" do
+        it "renders the index template" do
+            get :index #use get to call the index method of WelcomeController
+            expect(response).to render_template("index") #expect controller's response to be rendering index template
+        end
+    end
+
+    describe "GET about" do
+        it "renders the about template" do
+            get :about
+            expect(response).to render_template("about")
+        end
+    end
+
+end
