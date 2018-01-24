1ef5024495517ba10d25f89a5ddc8ad2f3531ebe
Chris Boatwright <christopher.boatwright@gmail.com>
2018-01-23 19:25:53 -0500

    Assigment Rails:Intro to CRUD Commit 01

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all #declare an instance variable @posts and assign it a collection of Post objects using the `all` method provided by ActiveRecord; `all` returns a collection of Post objects
+    @posts.map.with_index { |post, index| post.title.replace "SPAM" if (index == 0 || index % 5 == 0) }
   end
 
   def show
