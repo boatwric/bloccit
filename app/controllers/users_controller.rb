f293edcdd0a370f1554f4a2c0dca2077f8e688b7
Chris Boatwright <christopher.boatwright@gmail.com>
2018-02-07 20:40:00 -0500

    Checkpoint 24 Rails:Authentication-Signing Up Commit 01

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+#Controls User methods
+
+class UsersController < ApplicationController
+
+  #creates an instance variable named @user to be used by the new view's form
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    #create a new user with new and then set the corresponding attributes from the params hash
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    #save the new user to the database; add a flash message and redirect the user to the root path if save successful, display error and render new view if not
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
+
+  end
+
+end
